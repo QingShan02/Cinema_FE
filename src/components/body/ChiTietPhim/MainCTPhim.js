f3a353e1270153cf27484131ac21cf2823ee159e
khaiminh0401 <khaiminh0401@gmail.com>
2022-10-17 23:32:34 +0700

    hehe

--- src/components/body/ChiTietPhim/MainCTPhim.js
+++ src/components/body/ChiTietPhim/MainCTPhim.js
@@ -4,6 +4,13 @@ import { useLocation } from "react-router";
 import { useEffect } from "react";
 function MainCTPhim(props) {
 const data = useLocation();
+useEffect(() => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+  }, [])
 console.log(data);
     return ( <TieuDePhim maPhim={data.state.maPhim}/> );
 }
