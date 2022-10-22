d8dea0a11a1c35bb51f3e27816a207858c922985
khaiminh0401 <khaiminh0401@gmail.com>
2022-10-22 17:12:18 +0700

    route

--- src/components/TrangChu.js
+++ src/components/TrangChu.js
@@ -23,13 +23,13 @@ function TrangChu() {
 
         <div className="col-xs-10 col-sm-9 col-md-10 col-lg-10 bg-dark p-0">
           <Routes>
-            <Route path='/' exact element={<TrangChuComponent />}></Route>
-            <Route path='/pro' element={<MainKH />}></Route>
-            <Route path='/ctphim/*' element={<MainCTPhim />}></Route>
-            <Route path='/cn/*' element={<MainChoNgoi />}></Route>
-            <Route path='/dk/*' element={<MainDk />}></Route>
-            <Route path='/tp/*' element={<MainTP />}></Route>
-            <Route path='/thanhtoan/*' element={< MainThanhToan/>}></Route>
+            <Route path='RapChieuPhim_Web/' exact element={<TrangChuComponent />}></Route>
+            <Route path='RapChieuPhim_Web/pro' element={<MainKH />}></Route>
+            <Route path='RapChieuPhim_Web/ctphim/*' element={<MainCTPhim />}></Route>
+            <Route path='RapChieuPhim_Web/cn/*' element={<MainChoNgoi />}></Route>
+            <Route path='RapChieuPhim_Web/dk/*' element={<MainDk />}></Route>
+            <Route path='RapChieuPhim_Web/tp/*' element={<MainTP />}></Route>
+            <Route path='RapChieuPhim_Web/thanhtoan/*' element={< MainThanhToan/>}></Route>
 
           </Routes>
 
