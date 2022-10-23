88307eb98894ba690d55dc9fa025bc05d8990741
khaiminh0401 <khaiminh0401@gmail.com>
2022-10-23 15:26:15 +0700

    insert ve

--- src/components/body/ThanhToan/Bill.js
+++ src/components/body/ThanhToan/Bill.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import $ from 'jquery';
 function Bill() {
     let data = JSON.parse(sessionStorage.getItem("ghe"));
     let data1 = JSON.parse(sessionStorage.getItem("xuatchieu"));
+    const handleClick = ()=>{
+        $.ajax({
+            type: "get",
+            url: "https://localhost:8484/api/ve/insertVe",
+            data: {tongGiaVe:data.gia*1.05,thueVat:0.05,maCTGhe:0,maNV:''},
+            dataType: "json",
+            success: function (response) {
+                console.log(response);
+            }
+        });
+    }
     return (<div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
         <table className="table table-striped">
             <thead>
--- src/components/body/ThanhToan/HinhThuc.js
+++ src/components/body/ThanhToan/HinhThuc.js
@@ -1,7 +1,24 @@
 import React from 'react';
 function HinhThuc() {
     return (   <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-    Hình thức thanh toán
+    
+    <form action="/" method="POST" role="form">
+  <legend>Hình thức thanh toán</legend>
+  <div className="form-group">
+    <label htmlFor="ht">Ngân hàng</label>
+    
+    <div className="radio">
+  <label>
+    <input type="radio" name="ht" id="inputht" defaultValue="NCB" defaultChecked="checked" />
+    NCB
+  </label>
+</div>
+
+    
+  </div>
+</form>
+
+    
 </div>);
 }
 
