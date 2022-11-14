29a67d5c8cc824d7783dc8d0e5fac3cc2b44ff98
Khang Ho <hohoangkhang18@gmail.com>
2022-11-14 22:28:41 +0700

    chuc nang dang nhap bang fb

--- src/components/DangNhap.js
+++ src/components/DangNhap.js
@@ -4,12 +4,14 @@ import $ from 'jquery';
 import { Link, useLocation } from 'react-router-dom';
 import useCookie from 'react-use-cookie';
 import { useNavigate } from "react-router-dom";
-
+import FacebookLogin from 'react-facebook-login';
+import "./body/TrangChu/DangNhap/DangNhap.css";
 function DangNhap() {
   const [data, setData] = useState(false);
   const [inputs, setInputs] = useState({});
   const [cookie, setCookie] = useCookie("customer");
   const location = useLocation();
+  const [infoFB, setInfoFB] = useState({});
   // useEffect(()=>{
   //     handleSubmit();
   // },[]);
@@ -43,11 +45,40 @@ function DangNhap() {
     setInputs(values => ({ ...values, [name]: value }))
   };
 
+  const responseFacebook = async (response) => {
+    setInfoFB({
+      tenkh: response.name,
+      email: response.email,
+      idfb: response.id,
+      hinh: response.picture.data.url
+    });
+    console.log(infoFB);
+    $.ajax({
+      type: "get",
+      url: "http://localhost:8484/api/kh/insertKH",
+      data: infoFB,
+      dataType: "json",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (e) {
+        console.log(e);
+      }
+    });
+
+    console.log(infoFB);
+    console.log(response);
+  }
+
+  const componentClicked = (response) => {
+    console.log(response);
+  }
 
   return (
-    <div className="container-fluid  bg-white" style={{height:600}}>
+
+    <div className="container-fluid  bg-white" style={{ height: 600 }}>
       <p className="h1">Đăng nhập</p>
-      <hr/>
+      <hr />
       <form className="form-horizontal d-block mx-auto" onSubmit={handleSubmit}>
         <div className="form-group was-validated">
           {/* <label class="control-label col-sm-2" for="email"></label> */}
@@ -70,8 +101,20 @@ function DangNhap() {
         </div>
         <div className="form-group">
           <div className="col-sm-offset-2 col-sm-10">
-            <Link onClick={handleSubmit} className="btn btn-danger text-white" to="/"> Đăng Nhập</Link>
+            <Link onClick={handleSubmit} className="btn btn-danger text-white m-3" to="/"> Đăng Nhập</Link>
+
+            <FacebookLogin
+              appId="1293678041410090"
+              // autoLoad={true}
+              fields="name,email,picture"
+              onClick={componentClicked}
+              callback={responseFacebook}
+              size='medium'
+              icon="fa-facebook"
+              cssClass="my-facebook-button-class"
+            />
           </div>
+
         </div>
       </form>
     </div>
