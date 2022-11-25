3f0a9b6707f04b10c31c843970da832e77ed5eac
khaiminh0401 <khaiminh0401@gmail.com>
2022-11-25 12:47:10 +0700

    gop tra

--- src/components/body/ChiTietPhim/KhungGio.js
+++ src/components/body/ChiTietPhim/KhungGio.js
@@ -21,7 +21,7 @@ function KhungGio({maCN, maPhim}) {
     }, []);
 
     const a = data.map((s) => {
-         return  <button type="button" className="btn btn-outline-success">{s.gioBatDau}</button>
+         return  <button type="button" key={s.stt}className="btn btn-outline-success">{s.gioBatDau}</button>
     });
 
     return (
